a37521e43dd262edd9dc840e9efffd2c5c7bc04e
AClusive <99320710+AClusive@users.noreply.github.com>
2024-04-25 00:55:51 +0200

    Update handler.py

--- chatgpt_selenium_automation/handler.py
+++ chatgpt_selenium_automation/handler.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-class ChatGPTAutomation:
+class ChatGPTAutomation2:
 
     def __init__(self, chrome_path, chrome_driver_path):
         """
